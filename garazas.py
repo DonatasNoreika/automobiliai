63bca6424f583fd3d780d3a420dfb0259ba795fb
DonatasNoreika <donoras@gmail.com>
2025-05-29 11:33:20 +0300

    Pridėjau išsaugojimą į pickle

--- garazas.py
+++ garazas.py
@@ -1,9 +1,21 @@
-
+import pickle
 from models.elektromobilis import Automobilis, Elektromobilis
 
 class Garazas:
     def __init__(self):
-        self.automobiliai = []
+        self.automobiliai = self.gauti_garaza()
+
+    def gauti_garaza(self):
+        try:
+            with open("garazas.pkl", 'rb') as file:
+                automobiliai = pickle.load(file)
+        except:
+            automobiliai = []
+        return automobiliai
+
+    def irasyti_garaza(self):
+        with open("garazas.pkl", 'wb') as file:
+            pickle.dump(self.automobiliai, file)
 
     def prideti_auto(self):
         marke = input("Markė: ")
@@ -11,6 +23,7 @@ class Garazas:
         metai = int(input("Metai: "))
         automobilis = Automobilis(marke, modelis, metai)
         self.automobiliai.append(automobilis)
+        self.irasyti_garaza()
 
     def prideti_elektroauto(self):
         marke = input("Markė: ")
@@ -19,6 +32,7 @@ class Garazas:
         talpa = int(input("Talpa: "))
         elektroautomobilis = Elektromobilis(marke, modelis, metai)
         self.automobiliai.append(elektroautomobilis)
+        self.irasyti_garaza()
 
     def atvaizduoti_auto(self):
         # print(self.automobiliai)
